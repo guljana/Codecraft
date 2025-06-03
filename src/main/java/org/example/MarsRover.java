6ac8b40d16a49d9181a488ed6ebd2ae5f6707eaf
gsyed <guljana@gmail.com>
2025-06-03 12:48:57 +0100

    Added MarsRover tests

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -3,9 +3,129 @@ package org.example;
 
 public class MarsRover {
 
-    public static int[] moveRover(int[] ints, String n, String s)
+    private int x;
+    private int y;
+    private Character direction;
+
+    MarsRover(){
+        
+    }
+
+    public MarsRover(int x, int y, Character direction)
     {
-        return new int[]{0, 0}; // Placeholder implementation
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    private void moveForward()
+    {
+        switch (direction) {
+            case 'N':
+                y += 1;
+                break;
+            case 'S':
+                y -= 1;
+                break;
+            case 'E':
+                x += 1;
+                break;
+            case 'W':
+                x -= 1;
+                break;
+        }
+    }
+    private void moveBackward()
+    {
+        switch (direction) {
+            case 'N':
+                y -= 1;
+                break;
+            case 'S':
+                y += 1;
+                break;
+            case 'E':
+                x -= 1;
+                break;
+            case 'W':
+                x += 1;
+                break;
+        }
+    }
+    private void turnLeft()
+    {
+        switch (direction) {
+            case 'N':
+                direction = 'W';
+                break;
+            case 'W':
+                direction = 'S';
+                break;
+            case 'S':
+                direction = 'E';
+                break;
+            case 'E':
+                direction = 'N';
+                break;
+        }
+    }
+
+    public int[] getPosition()
+    {
+        return new int[]{x, y};
+    }
+
+    public void move(char dir)
+    {
+        if (dir == 'F') {
+            moveForward();
+        } else if (dir == 'B') {
+            moveBackward();
+            }
+        }
+
+    public void turn(Character turnDir)
+    {
+        switch (turnDir) {
+            case 'L':
+                switch (direction) {
+                    case 'N':
+                        direction = 'W';
+                        break;
+                    case 'W':
+                        direction = 'S';
+                        break;
+                    case 'S':
+                        direction = 'E';
+                        break;
+                    case 'E':
+                        direction = 'N';
+                        break;
+                }
+                break;
+            case 'R':
+                switch (direction) {
+                    case 'N':
+                        direction = 'E';
+                        break;
+                    case 'E':
+                        direction = 'S';
+                        break;
+                    case 'S':
+                        direction = 'W';
+                        break;
+                    case 'W':
+                        direction = 'N';
+                        break;
+                }
+                break;
+        }
+    }
+
+    public char getDirection()
+    {
+        return direction;
     }
 }
+
 
--- src/test/java/org/example/MarsRoverTest.java
+++ src/test/java/org/example/MarsRoverTest.java
@@ -7,8 +7,71 @@ public class MarsRoverTest {
 
     @Test
     void testRoverInitialPosition() {
-        MarsRover marsRover = new MarsRover();
-        assertArrayEquals(new int[]{0, 0}, MarsRover.moveRover(new int[]{0, 0}, "N", ""));
+        MarsRover marsRover = new MarsRover(0,0,'N');
+        assertArrayEquals(new int[]{0, 0}, marsRover.getPosition());
     }
+
+    @Test
+    void testRoverMoveForwardNorth(){
+        MarsRover marsRover = new MarsRover(0,0,'N');
+        marsRover.move('F');
+        assertArrayEquals(new int[]{0, 1}, marsRover.getPosition());
+    }
+    @Test
+    void testRoverMoveForwardSouth() {
+        MarsRover marsRover = new MarsRover(0,0,'S');
+        marsRover.move('F');
+        assertArrayEquals(new int[]{0, -1}, marsRover.getPosition());
+    }
+
+    @Test
+    void testRoverMoveForwardEast() {
+        MarsRover marsRover = new MarsRover(0,0,'E');
+        marsRover.move('F');
+        assertArrayEquals(new int[]{1, 0}, marsRover.getPosition());
+    }
+
+    @Test
+    void testRoverMoveForwardWest() {
+        MarsRover marsRover = new MarsRover(0,0,'W');
+        marsRover.move('F');
+        assertArrayEquals(new int[]{-1, 0}, marsRover.getPosition());
+    }
+
+    @Test
+    void testRoverMoveBackwardNorth() {
+        MarsRover marsRover = new MarsRover(0,0,'N');
+        marsRover.move('B');
+        assertArrayEquals(new int[]{0, -1}, marsRover.getPosition());
+    }
+
+    @Test
+    void testRoverMoveBackwardWest() {
+        MarsRover marsRover = new MarsRover(0,0,'W');
+        marsRover.move('B');
+        assertArrayEquals(new int[]{1, 0}, marsRover.getPosition());
+    }
+
+    @Test
+    void testRoverMoveBackwardEast() {
+        MarsRover marsRover = new MarsRover(0,0,'E');
+        marsRover.move('B');
+        assertArrayEquals(new int[]{-1, 0}, marsRover.getPosition());
+    }
+
+    @Test
+    void testRoverMoveBackwardSouth() {
+        MarsRover marsRover = new MarsRover(0,0,'S');
+        marsRover.move('B');
+        assertArrayEquals(new int[]{0, 1}, marsRover.getPosition());
+    }
+
+    @Test
+    void testRoverMoveLeftNorth() {
+        MarsRover marsRover = new MarsRover(0,0,'N');
+        marsRover.turn('L');
+        assertEquals('W', marsRover.getDirection());
+    }
+
 }
 
