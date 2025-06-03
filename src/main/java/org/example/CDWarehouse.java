f913db615ef907ee65c5f1b5bca68594fec4b7bf
gsyed <guljana@gmail.com>
2025-06-03 16:46:40 +0100

    Added CdwareHouse tests

--- src/main/java/org/example/CDWarehouse.java
+++ src/main/java/org/example/CDWarehouse.java
@@ -1,16 +1,38 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CDWarehouse
 {
   private List<CD> cds= new ArrayList<>();
+  private Map<CD, Integer> stock = new HashMap<>();
 
-    public void addCD(String title, String artist)
+    public void addCD(String title, String artist, int quantity)
     {
         CD cd = new CD(title, artist);
+        stock.put(cd, stock.getOrDefault(cd, 0) + quantity);
+    }
+    public void addCDs(CD cd, int quantity)
+    {
+        stock.put(cd, stock.getOrDefault(cd, 0) + quantity);
         cds.add(cd);
+    }
+
+    public int getStock(String title, String artist)
+    {
+        CD cd = new CD(title, artist);
+        return this.stock.get(cd);
+    }
+
+    public int getStock1(CD cd)
+    {
+        if (this.stock.containsKey(cd)) {
+            return this.stock.get(cd);
+        }
+        return 0;
     }
 
 
--- src/test/java/org/example/CDWarehouseTest.java
+++ src/test/java/org/example/CDWarehouseTest.java
@@ -7,17 +7,37 @@ public class CDWarehouseTest
     @Test
     public void addSingleCD(){
         CDWarehouse cdWarehouse = new CDWarehouse();
-        cdWarehouse.addCD("Thriller", "Mike");
-        assertEquals(1,cdWarehouse.getCds("Thriller", "Mike").size());
+       // cdWarehouse.addCD("Thriller", "Mike",1);
+        CD cd1 = new CD("The Wall", "Pink Floyd");
+        cdWarehouse.addCDs(cd1,1);
+        assertEquals(1, cdWarehouse.getStock1(cd1));
+        assertEquals(1,cdWarehouse.getCds("The Wall", "Pink Floyd").size());
     }
 
     @Test
     public void addMultipleCDsWithSameTitle(){
         CDWarehouse cdWarehouse = new CDWarehouse();
-        cdWarehouse.addCD("Thriller", "Mike");
-        cdWarehouse.addCD("Thriller", "Mike");
-        cdWarehouse.addCD("Thriller", "John");
-        assertEquals(2,cdWarehouse.getCds("Thriller", "Mike").size());
+        cdWarehouse.addCD("Thriller", "Mike",10);
+        assertEquals(10,cdWarehouse.getStock("Thriller", "Mike"));
+    }
+
+    @Test
+    public void addMultipleCDsWithDifferentTitle(){
+        CDWarehouse cdWarehouse = new CDWarehouse();
+        cdWarehouse.addCD("Thriller", "Mike",1);
+        cdWarehouse.addCD("Thriller", "John",1);
+        assertEquals(1,cdWarehouse.getCds("Thriller", "Mike").size());
         assertEquals(1,cdWarehouse.getCds("Thriller", "John").size());
     }
+
+    @Test
+    public void searchForCDsByTitle(){
+        CDWarehouse cdWarehouse = new CDWarehouse();
+        CD cd1 = new CD("The Wall", "Pink Floyd");
+        cdWarehouse.addCDs(cd1,1);
+        assertEquals(1, cdWarehouse.getCds("The Wall", "Pink Floyd").size());
+    }
+
+
+
 }
